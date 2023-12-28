a0966e6800feb621eb4e6b8f86e802bb73cc78b9
Johan Söderlund <johan.soderlund96@gmail.com>
2023-12-29 00:54:56 +0100

    code for fetching movie info from OMDb and creating HTML classes, if else

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// HTML IDs
 let movieNameRef = document.getElementById("movie-name");
 let searchBtn = document.getElementById("search-btn");
 let result = document.getElementById("result");
@@ -5,6 +6,7 @@ let result = document.getElementById("result");
 // Function to fetch data from OMDb API
 let getMovie = () => {
   let movieName = movieNameRef.value;
+  // Template URL for fetching movie name and API key for OMDb
   let url = `http://www.omdbapi.com/?t=${movieName}&apikey=${key}`;
 
   // If the input field is empty
@@ -13,44 +15,48 @@ let getMovie = () => {
     // If input field is not empty
   } else {
     fetch(url)
-      .then((resp) => resp.json())
+      .then((resp) => resp.json()) // Converting response to JSON
       .then((data) => {
         // If the movie exists in the OMDb database
-        if (data.Respone == "True") {
+        if (data.Response == "True") {
+          // Displays movie information and details by creating HTML classes
           result.innerHTML = `
-            <div class="info">
-            <img src=${data.Poster} class="poster">
-            <div>
-            <h2>${data.Title}</h2>
-            <div class="rating">
-            <img src="./images/star-icon.svg">
-            <h4>${data.imdbRating}</h4>
-            </div>
-            <div class="details">
-            <span>${data.Rated}</span>
-            <span>${data.Year}</span>
-            <span>${data.Runtime}</span>
-            </div>
-            <div class="genre">
-            <div>${data.Genre.split(",").join("</div><div>")}</div>
-            </div>
-            </div>
+                <div class="info">
+                    <img src=${data.Poster} class="poster">
+                    <div>
+                        <h2>${data.Title}</h2>
+                        <div class="rating">
+                            <img src="./images/star-icon.svg">
+                            <h4>${data.imdbRating}</h4>
+                    </div>
+                    <div class="details">
+                        <span>${data.Rated}</span>
+                        <span>${data.Year}</span>
+                        <span>${data.Runtime}</span>
+                    </div>
+                    <div class="genre">
+                        <div>${data.Genre.split(",").join("</div><div>")}</div>
+                    </div>
+                </div>
             </div>
             <h3>Plot:</h3>
             <p>${data.Plot}</p>
             <h3>Cast:</h3>
             <p>${data.Actors}</p>
             `;
+          // If the movie doesn't exists in the database
         } else {
           result.innerHTML = `<h3 class="msg">${data.Error}</h3>`;
         }
       })
-      // If error happens'
+      // If error happens
       .catch(() => {
         result.innerHTML = `<h3 class="msg">Error Occured</h3>`;
       });
   }
 };
 
+// Triggers getMovie function to allow search on click
 searchBtn.addEventListener("click", getMovie);
+// Starts eventlistener for when the window has loaded, triggers getMovie function
 window.addEventListener("load", getMovie);
