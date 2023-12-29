e5233ca4f75e31bd8cbc904f4f3436d729d7b2ff
Johan Söderlund <johan.soderlund96@gmail.com>
2023-12-29 01:05:11 +0100

    placeholder and comments

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ let getMovie = () => {
 
   // If the input field is empty
   if (movieName.length <= 0) {
-    result.innerHTML = `<h3 class="msg">Please enter a movie name </h3>`;
+    result.innerHTML = `<h3 class="msg">Please enter a movie, show or anime name </h3>`;
     // If input field is not empty
   } else {
     fetch(url)
